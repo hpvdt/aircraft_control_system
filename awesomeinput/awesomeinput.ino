232766764c2526c8f68b664d5a233874a5a1ccd3
danieltykei <55932890+danieltykei@users.noreply.github.com>
2023-01-24 20:24:52 -0500

    normalized resistance [-1,1]

--- awesomeinput/awesomeinput.ino
+++ awesomeinput/awesomeinput.ino
@@ -31,11 +31,14 @@ int sensorPin = A0;   // select the input pin for the potentiometer
 int sensorPin2 = A1;   // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
-const float slope = (0.00571429);
-const float yIntercept = 3.1429;
+
+const float top = 80; // * 1000s
+const float bottom = 13; // * 1000s
+
+
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //number of bits per second 
 }
 
 void loop() {
@@ -48,11 +51,20 @@ void loop() {
   reading = constrain(reading, -1, 1);
   reading2 = constrain(reading2,-1, 1);
 
-  Serial.print(getResistance(0));
+  Serial.print(convertResistance(0));
   Serial.print(",");
-  Serial.println(getResistance(1));
+  Serial.println(convertResistance(1));
   delay(100);
 }
+
+float convertResistance(int port) {
+
+  float slope = 0;
+  float resistorValue = getResistance(port);
+  slope = (2 / (top - bottom)) ;
+  return slope * (resistorValue / 1000) + 1 - (slope * top);
+}
+
 
 float getResistance(int port) {
   float resistance = 0;
