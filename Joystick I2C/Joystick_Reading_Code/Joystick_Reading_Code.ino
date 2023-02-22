9df57b279943e6e261c659d74036ea520f037c38
danieltykei <55932890+danieltykei@users.noreply.github.com>
2023-02-21 21:38:54 -0500

    String Data, 3 Values

--- Joystick I2C/Joystick_Reading_Code/Joystick_Reading_Code.ino
+++ Joystick I2C/Joystick_Reading_Code/Joystick_Reading_Code.ino
@@ -3,7 +3,7 @@
 
 
 Servo myservo;  // create servo object to control a servo
-float readings;
+float readings[3];
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
@@ -13,17 +13,56 @@ void setup() {
 }
 
 void loop() {
-
-  myservo.write(readings);                  // sets the servo position according to the scaled value
+  // myservo.write(0);
+  // Serial.println(readings[0]);
+  myservo.write(convertToServo(readings[2]));                  // sets the servo position according to the scaled value
   delay(100);
 }
 
 void receiveEvent() {
-  Serial.println("Received ");
-  readings = Wire.read();
-  Serial.println(readings); //Printing single character 1 byte or ( 8 bits )
+  
+  String data0 = "";
+  String data1 = "";
+  String data2 = "";
+
+  int dataWire = 1;
+  while( Wire.available()){
+    char dataChar = (char) Wire.read();
+    if (dataChar != (char) 'c') {
+      // Serial.print("Adding ");
+      // Serial.println(dataChar);
+      if (dataWire == 1) {
+        data0 += dataChar;
+        // Serial.println("updated data0");
+
+      }
+      else if (dataWire == 2) {
+        data1 += dataChar;
+        // Serial.println("updated data1");
+
+      }
+      else { //Data wire = 2
+        data2 += dataChar;
+        // Serial.println("updated data2");
+      }
+    }
+    else {
+      dataWire += 1;
+      // Serial.println("increased Wire");
+      // Serial.println(data0);
+      // Serial.println(data1);
+
+    }
+  }
+
+
+  readings[0] = data0.toFloat();
+  readings[1] = data1.toFloat();
+  readings[2] = data2.toFloat();
+ 
 }
 
 float convertToServo(float reading) {
-  return ((reading / 100 - 1) * 90  + 90);
+  // Serial.println((reading / 100 - 1) * 90  + 90);
+  return (reading * 90 + 89);
 }
